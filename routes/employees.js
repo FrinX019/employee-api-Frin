b16cd1272a11ffafa006a01cbaa2ca39b92b5393
frinx019 <frinpatel1841974@gmail.com>
2025-11-20 13:39:37 -0500

    Added comment declaration

--- routes/employees.js
+++ routes/employees.js
@@ -1,3 +1,17 @@
+/******************************************************************************
+***
+* ITE5315 – Assignment 4
+* I declare that this assignment is my own work in accordance with Humber Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Frin Sureshbhai Patel Student ID: N01680041 Date: Nov 19,2025
+*
+*
+******************************************************************************
+**/
+
+
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/employeeController");
